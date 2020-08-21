a50f32c1dcddafa4a74dde78f1464db9ec1cb375
MelaniaBB <69200779+MelaniaBB@users.noreply.github.com>
2020-08-21 02:38:52 -0300

    Update webhook.php

--- mp/webhook.php
+++ mp/webhook.php
@@ -7,12 +7,12 @@ require($_SERVER['DOCUMENT_ROOT']. '/mp/credencialesMP.php');
     //MercadoPago\SDK::setAccessToken("ENV_ACCESS_TOKEN");
 $fh = fopen("logMP.txt", 'a') or die("Se produjo un error al crear el archivo");
 
-$texto = date('Y-m-d h:i:sa')." --> TOPIC: ".$_GET["topic"]." --> ID: ".$_GET["id"];
+$texto = date('Y-m-d h:i:sa')." --> TOPIC: ".$_POST["type"]." --> ID: ".$_POST["id"];
 
 fwrite($fh, $texto.PHP_EOL) or die("No se pudo escribir en el archivo");
 
 fclose($fh);
-    switch($_GET["topic"]) {
+    switch($_POST["type"]) {
         case "payment":
             $payment = MercadoPago\Payment.find_by_id($_POST["id"]);
             break;
