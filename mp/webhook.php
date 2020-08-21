dfca88b171858a0a73b538a893917dd88fac1023
MelaniaBB <69200779+MelaniaBB@users.noreply.github.com>
2020-08-21 03:37:12 -0300

    Update webhook.php

--- mp/webhook.php
+++ mp/webhook.php
@@ -8,13 +8,7 @@ require($_SERVER['DOCUMENT_ROOT']. '/mp/credencialesMP.php');
 
 
 //$info = json_decode($post);
-$fh = fopen("logMP.txt", 'a') or die("Se produjo un error al crear el archivo");
 
-//$data = json_decode(file_get_contents($_GET), true);
-
-
-
-fclose($fh);
     switch($_GET["topic"]) {
         case "payment":
             $payment = MercadoPago\Payment.find_by_id($_POST["id"]);
@@ -32,7 +26,13 @@ fclose($fh);
 			$merchant_order = MercadoPago\MerchantOrder::find_by_id($_GET["id"]);
 			break;	
     }
+$fh = fopen("logMP.txt", 'a') or die("Se produjo un error al crear el archivo");
+
+//$data = json_decode(file_get_contents($_GET), true);
+
 $texto = date('Y-m-d h:i:sa')." --> TOPIC: ".$_GET["topic"]." --> ID: ".$_GET["id"]." --> ".$merchant_order;
 
 fwrite($fh, $texto.PHP_EOL) or die("No se pudo escribir en el archivo");
+
+fclose($fh);
 ?>
