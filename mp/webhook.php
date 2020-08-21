3c7a230743fec08d72379601b906331840a314a4
MelaniaBB <69200779+MelaniaBB@users.noreply.github.com>
2020-08-21 02:53:45 -0300

    Update webhook.php

--- mp/webhook.php
+++ mp/webhook.php
@@ -10,7 +10,9 @@ require($_SERVER['DOCUMENT_ROOT']. '/mp/credencialesMP.php');
 //$info = json_decode($post);
 $fh = fopen("logMP.txt", 'a') or die("Se produjo un error al crear el archivo");
 
-$texto = date('Y-m-d h:i:sa')." --> TOPIC: ".$_POST["type"]." --> ID: ".$_POST["id"]." --> ".$_POST;
+$data = json_decode(file_get_contents($_POST), true);
+
+$texto = date('Y-m-d h:i:sa')." --> ".$data;
 
 fwrite($fh, $texto.PHP_EOL) or die("No se pudo escribir en el archivo");
 
